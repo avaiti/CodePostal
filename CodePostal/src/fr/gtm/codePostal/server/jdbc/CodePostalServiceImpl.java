aa907b277d5c7df2b6a618a5470e7a6e50e5f75c
Adminl <Adminl@Parme61>
2016-07-26 15:51:43 +0200

    service + xml

--- CodePostal/src/fr/gtm/codePostal/server/jdbc/CodePostalServiceImpl.java
+++ CodePostal/src/fr/gtm/codePostal/server/jdbc/CodePostalServiceImpl.java
@@ -1,5 +1,6 @@
 package fr.gtm.codePostal.server.jdbc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -7,10 +8,11 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import fr.gtm.codePostal.client.CodePostalService;
 
 public class CodePostalServiceImpl extends RemoteServiceServlet implements CodePostalService {
-
+	private static List<Ville> villes = new ArrayList<Ville>();
+	
 	@Override
 	public List<Ville> getVillesByCodePostal(String cp) {
-		return null;
+		return villes;
 	}
 
 }
