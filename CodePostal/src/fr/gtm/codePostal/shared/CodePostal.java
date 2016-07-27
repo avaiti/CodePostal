8fcb946397b904332c2b6e6210960ecfa78a87bc
Sebastien <campacid@gmail.com>
2016-07-27 09:06:17 +0200

    suppression warnings

--- CodePostal/src/fr/gtm/codePostal/shared/CodePostal.java
+++ CodePostal/src/fr/gtm/codePostal/shared/CodePostal.java
@@ -3,13 +3,11 @@ package fr.gtm.codePostal.shared;
 import java.util.List;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.TextBox;
